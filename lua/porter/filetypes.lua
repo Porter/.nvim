4f999c2f91edcc002be5eb2192f009f29314fad1
PorterAtGoogle <phaet@google.com>
2025-03-11 04:14:49 +0000

    filetype stuff

--- lua/porter/filetypes.lua
+++ lua/porter/filetypes.lua
@@ -1,8 +1,30 @@
 local M = {}
 
 function M.setup(opts)
+        opts['filetypes'] = opts['filetypes'] or {}
         for _, ft in ipairs(opts['filetypes']) do
                 vim.filetype.add(ft)
+        end
+
+        opts['filetypeOpts'] = opts['filetypeOpts'] or {}
+        for _, fto in ipairs(opts['filetypeOpts']) do
+                for file, opts in pairs(fto) do
+                        local optsGroup = vim.api.nvim_create_augroup(file .. "Opts", {
+                                clear = true
+                        })
+                        vim.api.nvim_create_autocmd("FileType", {
+                                callback = function ()
+                                        if opts['spaces'] ~= nil and opts['spaces'] == false then
+                                                vim.opt_local.expandtab = false
+                                        end
+                                        if opts['shiftwidth'] ~= nil then
+                                                vim.opt_local.shiftwidth = opts['shiftwidth']
+                                        end
+                                end,
+                                pattern = file,
+                                group = optsGroup,
+                        })
+                end
         end
 end
 
